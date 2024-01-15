3fe7031aab595908b7924e49f95fb00ac6ddb4a0
Idil Kiran <idil.kiran@code.berlin>
2024-01-15 22:53:39 +0100

    change 1

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
+import { defineConfig } from "vite"
+import vue from '@vitejs/plugin-vue'
+
+
+
 export default {
     // config options
-    base: '/idilkiran/',
+    base: '/soundofviolence/',
+    plugins: [vue()],
   }
